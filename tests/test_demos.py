95c4fc3436d39cca69ae99f5d24099b070677b53
manios krasanakis <maniospas@hotmail.com>
2023-06-15 12:57:41 +0300

    updated demo tests

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -1,5 +1,12 @@
 import fairbench as fb
+from .test_forks import environment
 
 
-def test_adult():
-    fb.demos.adult()
+def test_settings(monkeypatch):
+    from matplotlib import pyplot as plt
+    monkeypatch.setattr(plt, 'show', lambda: None)
+    for _ in environment():
+        for setting, protected in [(fb.demos.adult, 8), (fb.demos.bank, "marital")]:
+            test, y, yhat = setting()
+            sensitive = fb.Fork(fb.categories@test[protected])
+            fb.visualize(fb.multireport(predictions=yhat, labels=y, sensitive=sensitive))
