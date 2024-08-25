0d355a0e8e5b0f72de5e71fc48ef50ab59af7ad0
maniospas <maniospas@hotmail.com>
2024-08-25 13:37:45 +0300

    fixed broken coverage badge link

--- examples/playground/example.py
+++ examples/playground/example.py
@@ -4,9 +4,13 @@ import fairbench as fb
 test, y, yhat = fb.demos.adult(predict="probabilities")
 s = fb.Fork(fb.categories @ test[9])  # test[8] is a pandas column with race values
 
-report = fb.multireport(
+report1 = fb.multireport(
     scores=yhat, predictions=(yhat > 0.5), labels=y, sensitive=s, top=50
 )
+report2 = fb.unireport(
+    scores=yhat, predictions=(yhat > 0.5), labels=y, sensitive=s, top=50
+)
+report = fb.combine(report1, report2)
 fb.describe(report)
 
 # report = fb.accreport(
