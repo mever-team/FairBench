ba08ddf2e18efc4001d21ef8af33d43c888e1f8a
maniospas <maniospas@hotmail.com>
2024-12-19 11:16:21 +0200

    some tests

--- playground/objworks/basics.py
+++ playground/objworks/basics.py
@@ -1,8 +1,9 @@
 import os
 
 os.environ["FBINTERACTIVE"] = "True"
-import fairbench as fb
 
+
+import fairbench as fb
 
 x, y, yhat = fb.bench.tabular.compas(test_size=0.5)
 print(x)
@@ -11,8 +12,11 @@ print(x)
 sensitive = fb.Dimensions(fb.categories @ x["sex"], fb.categories @ x["race"])
 print(sensitive)
 
+sensitive = sensitive.intersectional()
+print(sensitive)
 
-report = fb.reports.pairwise(
-    predictions=fb.categories @ yhat, labels=fb.categories @ y, sensitive=sensitive
-)
-report.show(fb.export.HtmlTable())
+report = fb.reports.pairwise(predictions=yhat, labels=y, sensitive=sensitive)
+
+report.filter(fb.investigate.Stamps).show(env=fb.export.Html, depth=1)
+
+report.help()
