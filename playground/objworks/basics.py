9d198a6df1b201a3634467ab6fc355292bdc1e69
maniospas <maniospas@hotmail.com>
2025-03-19 10:57:06 +0200

    added example to readme

--- playground/objworks/basics.py
+++ playground/objworks/basics.py
@@ -3,12 +3,6 @@ import fairbench as fb
 x, y, yhat = fb.bench.tabular.compas(test_size=0.5)
 
 sensitive = fb.Dimensions(fb.categories @ x["sex"], fb.categories @ x["race"])
-sensitive = sensitive.intersectional()
+sensitive = sensitive.intersectional().strict()
 report = fb.reports.pairwise(predictions=yhat, labels=y, sensitive=sensitive)
-
-report.filter(fb.investigate.Stamps).show(
-    env=fb.export.Html(distributions=False), depth=1
-)
-report.show(env=fb.export.ConsoleTable)
-
-report.maxrel.tnr.show()
+report.filter(fb.investigate.Stamps).show(env=fb.export.Html(horizontal=True), depth=1)
