1ae6177d26ebbde452604a82121c25e57f7a353b
maniospas <maniospas@hotmail.com>
2026-04-15 08:22:57 +0300

    streamlined some objworks tests

--- playground/objworks/stamps.py
+++ playground/objworks/stamps.py
@@ -6,32 +6,15 @@ x, y, yhat = deprecated.bench.tabular.bank()
 sensitive = fb.Dimensions(
     {"marital": fb.categories @ x["marital"], "age": fb.fuzzy @ x["age"]}
 )
-print(sensitive.branches)
+print(sensitive.branches().keys())
 report = fb.reports.pairwise(
     sensitive=sensitive,
     predictions={"true": yhat, "false": 1 - yhat},
     labels={"true": y, "false": 1 - y},
 )
 
-report.filter(fb.investigate.Stamps, fb.investigate.Worst).show(
+print(report.filter(fb.investigate.Stamps)) # summary print
+report.filter(fb.investigate.Stamps).show(
     env=fb.export.Html, depth=2
-)
-print(report.filter(fb.investigate.Stamps))
+) # show stamps as html
 
-report.filter(fb.investigate.Stamps).show()
-
-
-"""
-print(comparison)
-
-value = (comparison
-          |fb.reduction.minimum
-          |fb.measures.pr)
-
-print(value)
-
-for v in value.flatten():
-    print(v)
-"""
-
-# print(json.dumps((report_over_time|pr|minimum).serialize(), indent="  "))
